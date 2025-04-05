922fa51f4d3bcefecd9213a63260bf3e980b741c
Ivan63-coder <ivanyudin28@gmail.com>
2025-04-05 15:52:48 +0400

    add brake_even & asciinema

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 
 ### Maintainability Badge:
  <a href="https://codeclimate.com/github/Ivan63-coder/python-project-49/maintainability"><img src="https://api.codeclimate.com/v1/badges/d758bfcf2b53f4ddf9e1/maintainability" /></a>
+
+### Asciinema:
+https://asciinema.org/connect/928c732c-9b52-4725-8954-37a6d53271df
--- brain_games/module.py
+++ brain_games/module.py
@@ -1,2 +1,27 @@
+import random
+
+import prompt
+
+
 def brain_games():
     print('Welcome to the Brain Games!')
+
+
+def brain_even():
+    brain_games()
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    for _ in range(3):
+        number = random.randint(1, 100)
+        print(f'Question: {number}')
+        answer = input('Your answer: ').strip().lower()
+        correct = 'yes' if number % 2 == 0 else 'no'
+
+        if answer != correct:
+            print(f"'{answer}' is wrong answer ;(."
+                  f" Correct answer was '{correct}'.")
+            print(f"Let's try again, {name}!")
+            return
+        print("Correct!")
+    print(f"Congratulations, {name}!")
