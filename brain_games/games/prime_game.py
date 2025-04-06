caaa506076d965e3887abeec6741756cfbe39e9e
Ivan63-coder <ivanyudin28@gmail.com>
2025-04-06 17:07:28 +0400

    fixed brain-prime

--- brain_games/games/prime_game.py
+++ brain_games/games/prime_game.py
@@ -7,8 +7,8 @@ def is_prime(n):
     for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
             return False
-        return True
-
+    return True
+        
 
 def prime_game():
     question = random.randint(1, 100)
